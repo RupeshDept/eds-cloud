46e3905707274507e9ea68f801351d69a4f25c85
RupeshDept <rupesh.mahadik@deptagency.com>
2025-04-10 17:05:55 +0530

    added pms navbar block

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,12 @@
 // add delayed functionality here
+{/* <script src="https://js.sentry-cdn.com/955e247a0268fb1c7c06c26e56059c49.min.js" crossorigin="anonymous">
+
+    Sentry.onLoad(function () {
+        Sentry.init({
+            // You can add any additional configuration here
+
+        })
+    myUndefinedFunction();
+});
+</script>
+ */}
