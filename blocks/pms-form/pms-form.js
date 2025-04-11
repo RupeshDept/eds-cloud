72ea2ad56d5e19ed140a5ae2fc9db4d9ec25db19
RupeshDept <rupesh.mahadik@deptagency.com>
2025-04-11 13:08:36 +0530

    added form via api successfully

--- blocks/pms-form/pms-form.js
+++ blocks/pms-form/pms-form.js
@@ -1,4 +1,9 @@
+import myform from "../form/form.js"
+
 export default function decorate(block) {
-    const path = block.children
+    const path = block.textContent.trim()
+    document.querySelector('.pms-form a').textContent = ""
     console.log(path)
+    myform(block, path)
+
 }
