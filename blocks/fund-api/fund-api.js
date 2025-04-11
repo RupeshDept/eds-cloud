0232167534adeb2acc9d78350f55f4cc284248ac
RupeshDept <rupesh.mahadik@deptagency.com>
2025-04-11 15:58:24 +0530

    added place holder

--- blocks/fund-api/fund-api.js
+++ blocks/fund-api/fund-api.js
@@ -1,4 +1,32 @@
+import { fetchAPI } from "../../scripts/scripts.js";
+
 export default function decorate(block) {
     const path = block.textContent.trim()
     console.log(path);
+
+
+
+    function fundAPI() {
+        const apiRequest = {
+            "api_name": "FundCategoryNew",
+            "categoryType": "MF"
+        }
+        const apiid = "27820BB4MEC3DA4D65MAC74CDFF81E020A60"
+        const APIURL = "https://mf.moamc.com/mutualfund/api/v1/getAllFundsBoost"
+        const requestObj = {
+            requestJson: apiRequest,
+            headerJson: {
+                appid: apiid,
+            },
+        };
+
+        return new Promise((resolve, reject) => {
+            fetchAPI('POST', APIURL, requestObj)
+                .then((response) => {
+                    console.log("Data inserted successfully.");
+                    // resolve('Data inserted successfully.');
+                });
+        });
+    }
+    fundAPI()
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -145,3 +145,39 @@ async function loadPage() {
 }
 
 loadPage();
+
+
+/// API ///
+export function fetchAPI(method, url, data) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (method === 'GET') {
+        const resp = await fetch(url);
+        resolve(resp);
+      } else if (method === 'POST') {
+        data.headerJson = data.headerJson || {
+          'Content-Type': 'application/json',
+        };
+        if (data.headerJson['Content-Type'] == 'remove') {
+          data.headerJson['Content-Type'] = '';
+        } else {
+          data.headerJson['Content-Type'] = data.headerJson['Content-Type'] ? data.headerJson['Content-Type'] : 'application/json';
+        }
+        /* Optimzie Code */
+        /* data.headerJson = data.headerJson || {};
+        data.headerJson["Content-Type"] = data.headerJson["Content-Type"] === 'remove' ? '' : data.headerJson["Content-Type"] || "application/json"; */
+        const request = new Request(url, {
+          method: 'POST',
+          body: JSON.stringify(data.requestJson),
+          headers: data.headerJson,
+        });
+        const response = await fetch(request);
+        const json = await response.json();
+        resolve({ responseJson: json });
+      }
+    } catch (error) {
+      console.warn(error);
+      reject(error);
+    }
+  });
+}
