a0ce89f56ab3db3f07c620dab1674a49bced06d0
RupeshDept <rupesh.mahadik@deptagency.com>
2025-06-25 17:06:08 +0530

    added explore our products block

---
+++ blocks/explore-products-right-cards/explore-products-right-cards.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+export default function decorate() {
+    var rightCards = document.querySelector(".explore-products-right-cards");
+    Array.from(rightCards.children).forEach((e, i) => {
+        e.classList.add("right-card", `right-card-${i + 1}`);
+        Array.from(e.children).forEach((e, i) => {
+            e.classList.add(`card-item-${i + 1}`);
+        });
+    });
+};
