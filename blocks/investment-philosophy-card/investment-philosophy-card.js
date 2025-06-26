3b44308f556a07a7139c96bf58b0a9b7ae84be79
RupeshDept <rupesh.mahadik@deptagency.com>
2025-06-26 16:52:55 +0530

    added 4 investment index-page

---
+++ blocks/investment-philosophy-card/investment-philosophy-card.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".investment-philosophy-card").forEach((e, i) => {
+    Array.from(e.children).forEach((e) => {
+        e.classList.add('card-item');
+        Array.from(e.children)[0].classList.add("item-icon");
+        Array.from(e.children)[1].classList.add("item-content");
+    });
+});
