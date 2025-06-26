7b629e8f9aafddf3e43db5d5ef075d5dd7b2c80b
RupeshDept <rupesh.mahadik@deptagency.com>
2025-06-26 12:03:27 +0530

    added 3 strategy index-page

---
+++ blocks/strategy-card/strategy-card.js
@@ -0,0 +1,7 @@
+Array.from(document.querySelector(".strategy-card").children).forEach(
+    (e, i) => {
+        e.classList.add(`card-item-${i + 1}`, 'card-item');
+        Array.from(e.children)[0].classList.add('item-icon')
+        Array.from(e.children)[1].classList.add('item-content')
+    }
+);
