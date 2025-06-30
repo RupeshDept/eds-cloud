a6256e381dfff7e29fb3d81ba4cc8d1c79c15ab9
RupeshDept <rupesh.mahadik@deptagency.com>
2025-06-30 19:24:30 +0530

    cacl code one

--- blocks/performance-graph/performance-graph.js
+++ blocks/performance-graph/performance-graph.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 // import { div, img, h2, p, a } from '../../scripts/dom-helpers.js';
-import data from './data.json';
+import chartsData from "./data.js";
 export default function decorate(block) {
-
 
     // Create a new div element to hold the graph
     const graphDiv = document.createElement('div');
@@ -15,28 +14,36 @@ export default function decorate(block) {
 
     const btn = document.createElement('button');
     btn.textContent = 'Show Graph';
+    const useLiveAPI = false;
     btn.addEventListener('click', () => {
         // showGraph();
-        const requestData = {
-            api_name: "PerformanceGraphNew",
-            cmt_schcode: "24097",
-            graphType: "Lumpsum",
-            invamount: "10000",
-            isCompare: "",
-            isin: "INF247L01445",
-            schcode: "FM"
-        };
-        myAPI('POST', 'https://www.motilaloswalmf.com/mutualfund/api/v1/PerformanceGraphNew', requestData)
-            .then(data => {
-                localStorage.setItem("chartData", JSON.stringify(data));
-                showGraph();
-            })
-            .catch(error => {
-                console.error("Error fetching data:", error);
-                showGraph()
 
-            });
+        if (useLiveAPI) {
+            const requestData = {
+                api_name: "PerformanceGraphNew",
+                cmt_schcode: "24097",
+                graphType: "Lumpsum",
+                invamount: "10000",
+                isCompare: "",
+                isin: "INF247L01445",
+                schcode: "FM"
+            };
+            myAPI('POST', 'https://www.motilaloswalmf.com/mutualfund/api/v1/PerformanceGraphNew', requestData)
+                .then(data => {
+                    localStorage.setItem("chartData", JSON.stringify(data));
+                    showGraph();
+                })
+                .catch(error => {
+                    console.error("Error fetching data:", error);
+                    showGraph()
+
+                });
+
+        } else {
+            showGraph()
+        }
     });
+
     btn.classList.add('show-graph-button');
     block.appendChild(btn);
 
@@ -73,16 +80,40 @@ export default function decorate(block) {
         });
     }
 
+    async function showGraph() {
+        try {
+            const parsedChartData = chartsData || JSON.parse(localStorage.getItem("chartData"));
+            if (!parsedChartData) {
+                console.error("No chart data available.");
+                return;
+            }
+            const key = Object.keys(parsedChartData)[0];
+            const chartArray = parsedChartData[key];
 
-    function showGraph() {
+            const chartData = chartArray.map((item) => {
+                return {
+                    date: new Date(item.navdate).getTime(),
+                    value1: parseFloat(item.marketValue_Scheme),
+                    value2: parseFloat(item.marketValue_BM1),
+                };
+            });
+
+            // rendering chart using this chartData
+            renderChart(chartData);
+        } catch (error) {
+            console.error("Error loading chart data:", error);
+        }
+    }
+
+    function renderChart(chartData) {
         window.am5.ready(() => {
-            var root = window.am5.Root.new("chartdiv");
+            const root = window.am5.Root.new("chartdiv");
 
             root.setThemes([
                 window.am5themes_Animated.new(root)
             ]);
 
-            var chart = root.container.children.push(window.am5xy.XYChart.new(root, {
+            const chart = root.container.children.push(window.am5xy.XYChart.new(root, {
                 panX: true,
                 panY: true,
                 wheelX: "panX",
@@ -92,10 +123,10 @@ export default function decorate(block) {
 
             chart.get("colors").set("step", 3);
 
-            var cursor = chart.set("cursor", window.am5xy.XYCursor.new(root, {}));
+            const cursor = chart.set("cursor", window.am5xy.XYCursor.new(root, {}));
             cursor.lineY.set("visible", false);
 
-            var xAxis = chart.xAxes.push(window.am5xy.DateAxis.new(root, {
+            const xAxis = chart.xAxes.push(window.am5xy.DateAxis.new(root, {
                 maxDeviation: 0.3,
                 baseInterval: {
                     timeUnit: "day",
@@ -105,16 +136,15 @@ export default function decorate(block) {
                 tooltip: window.am5.Tooltip.new(root, {})
             }));
 
-            var yAxis = chart.yAxes.push(window.am5xy.ValueAxis.new(root, {
+            const yAxis = chart.yAxes.push(window.am5xy.ValueAxis.new(root, {
                 maxDeviation: 0.3,
                 renderer: window.am5xy.AxisRendererY.new(root, {})
             }));
 
-            // Series 1 - Scheme
-            var series = chart.series.push(window.am5xy.LineSeries.new(root, {
+            const series = chart.series.push(window.am5xy.LineSeries.new(root, {
                 name: "Motilal Oswal Midcap Fund",
-                xAxis: xAxis,
-                yAxis: yAxis,
+                xAxis,
+                yAxis,
                 valueYField: "value1",
                 valueXField: "date",
                 tensionX: 0.8,
@@ -123,28 +153,20 @@ export default function decorate(block) {
                 })
             }));
 
-            series.strokes.template.setAll({
-                strokeWidth: 2
-            });
+            series.strokes.template.setAll({ strokeWidth: 2 });
+            series.bullets.push(() => window.am5.Bullet.new(root, {
+                sprite: window.am5.Circle.new(root, {
+                    radius: 4,
+                    fill: series.get("stroke"),
+                    stroke: root.interfaceColors.get("background"),
+                    strokeWidth: 2
+                })
+            }));
 
-            series.get("tooltip").get("background").set("fillOpacity", 0.5);
-
-            series.bullets.push(() => {
-                return window.am5.Bullet.new(root, {
-                    sprite: window.am5.Circle.new(root, {
-                        radius: 4,
-                        fill: series.get("stroke"),
-                        stroke: root.interfaceColors.get("background"),
-                        strokeWidth: 2
-                    })
-                });
-            });
-
-            // Series 2 - Benchmark
-            var series2 = chart.series.push(window.am5xy.LineSeries.new(root, {
+            const series2 = chart.series.push(window.am5xy.LineSeries.new(root, {
                 name: "Nifty Midcap 150 TRI",
-                xAxis: xAxis,
-                yAxis: yAxis,
+                xAxis,
+                yAxis,
                 valueYField: "value2",
                 valueXField: "date",
                 tensionX: 0.8
@@ -155,60 +177,29 @@ export default function decorate(block) {
                 strokeWidth: 2
             });
 
-            series2.bullets.push(() => {
-                return window.am5.Bullet.new(root, {
-                    sprite: window.am5.Circle.new(root, {
-                        radius: 4,
-                        fill: series2.get("stroke"),
-                        stroke: root.interfaceColors.get("background"),
-                        strokeWidth: 2
-                    })
-                });
-            });
+            series2.bullets.push(() => window.am5.Bullet.new(root, {
+                sprite: window.am5.Circle.new(root, {
+                    radius: 4,
+                    fill: series2.get("stroke"),
+                    stroke: root.interfaceColors.get("background"),
+                    strokeWidth: 2
+                })
+            }));
 
             root.dateFormatter.setAll({
                 dateFormat: "yyyy-MM-dd",
                 dateFields: ["valueX"]
             });
 
-
-
-            // const storedChartData = localStorage.getItem("chartData");
-            // const parsedChartData = JSON.parse(storedChartData);
-            console.log("Data from JSON Graph:", data);
-
-            const parsedChartData = data; // Use the imported data directly
-            const key = Object.keys(parsedChartData)[0];
-            const chartArray = parsedChartData[key];
-            // const first10 = chartArray.slice(0, 10);
-            console.log(chartArray);
-
-
-            const chartData = chartArray.map((item) => {
-                return {
-                    date: new Date(item.navdate).getTime(),
-                    value1: parseFloat(item.marketValue_Scheme),
-                    value2: parseFloat(item.marketValue_BM1),
-                };
-            });
-
             series.data.setAll(chartData);
             series2.data.setAll(chartData);
 
-            // Add legend
-            var legend = chart.children.push(window.am5.Legend.new(root, {
+            const legend = chart.children.push(window.am5.Legend.new(root, {
                 centerX: window.am5.p50,
                 x: window.am5.p50
             }));
 
-            // tooltip: window.am5.Tooltip.new(root, {
-            //     labelText: "{name}: {valueY}"
-            // })
-
-
             legend.data.setAll(chart.series.values);
-
-
 
             series.appear(1000);
             series2.appear(1000);
