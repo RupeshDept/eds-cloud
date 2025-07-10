c1525aed76e399f0ff7330e1bf6116ea9e92c726
RupeshDept <rupesh.mahadik@deptagency.com>
2025-07-10 16:02:30 +0530

    added hero-section

--- blocks/hero-section/hero-section.js
+++ blocks/hero-section/hero-section.js
@@ -1,16 +1,16 @@
 /* eslint-disable */
 
 export default function decorate(block) {
-    const config = block.dataset || {};
+  const config = block.dataset || {};
 
-    const title = config.title || 'Power of Compounding Starts Here';
-    const subtitle = config.subtitle || 'Explore how much wealth you would have created by investing in our schemes using this SIP Calculator';
-    const icon = config.icon || '📊';
+  const title = config.title || 'Power of Compounding Starts Here';
+  const subtitle = config.subtitle || 'Explore how much wealth you would have created by investing in our schemes using this SIP Calculator';
+  const icon = config.image || '📊';
 
-    block.innerHTML = `
+  block.innerHTML = `
     <div class="hero-section">
       <div class="hero-content">
-        <div class="hero-icon">${icon}</div>
+        <div class="hero-icon"><img src=${icon} alt="img alt" /></div>
         <div class="hero-text">
           <h1 class="hero-title">${title}</h1>
           <p class="hero-subtitle">${subtitle}</p>
