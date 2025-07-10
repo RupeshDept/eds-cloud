d31370f2b970795c50427cd2b2b235770c45af9c
RupeshDept <rupesh.mahadik@deptagency.com>
2025-07-10 13:43:36 +0530

    main reboot

--- blocks/pms-form/pms-form.js
+++
@@ -1,9 +0,0 @@
-import myform from "../form/form.js"
-
-export default function decorate(block) {
-    const path = block.textContent.trim()
-    document.querySelector('.pms-form a').textContent = ""
-    console.log(path)
-    myform(block, path)
-
-}
