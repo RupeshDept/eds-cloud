629f53a5db687b6294d014455d95a6abb4e2dccd
RupeshDept <rupesh.mahadik@deptagency.com>
2025-07-14 12:15:07 +0530

    added css for compouding block

--- blocks/calculator-sip/calculator-sip.js
+++ blocks/calculator-sip/calculator-sip.js
@@ -1,10 +1,78 @@
 /*eslint-disable */
-
+import { div, a, label, h4, input, span, button } from '../../scripts/dom-helpers.js';
 export default function decorate(block) {
     console.log('Calculator SIP block loaded', block);
-    const config = block.dataset || {};
+    // const config = block.dataset || {};
+    Array.from(block.children).forEach((child, index) => {
+        child.classList.add('calContainer' + (index + 1));
+    });
+    let calContainer = div({ class: 'calContainer' },
+        div({ class: "searchBarchWrapper" },
+            span(block.querySelector('.calContainer1 p').textContent.trim()),
+            input({
+                value: block.querySelector('.calContainer2 p').textContent.trim(),
+                type: "text",
+                placeholder: "Search a fund",
+                name: "searchFundInput",
+                id: "searchFundInput",
+            }),
+        ),
+        div({ class: "schemeBtnsWrapper" },
+            button(block.querySelector('.calContainer3 p').textContent.trim()),
+            button(block.querySelector('.calContainer4').textContent.trim()),
+        ),
+        div({ class: "investmentWrapper" },
+            div({ class: "sipWrapper" },
+                label({ class: "labelforsip" }, block.querySelector('.calContainer5').textContent.trim()),
+                label({ class: "labelforlumsum", style: "display:none" }, block.querySelector('.calContainer6').textContent.trim()),
+                input({
+                    type: "number",
+                    value: block.querySelector('.calContainer7').textContent.trim(),
+                    name: "investmentAmount",
+                    id: "investmentAmount",
+                    placeholder: "Enter amount",
+                }),
+            ),
+            div({ class: "tenureWrapper" },
+                label(block.querySelector('.calContainer8').textContent.trim()),
+                input({
+                    type: "number",
+                    value: block.querySelector('.calContainer9').textContent.trim(),
+                    name: "investmentTenure",
+                    id: "investmentTenure",
+                    placeholder: "Enter tenure in years",
+                }),
+            )
+        ),
+        div({ class: "caldiscription" },
+            div({ class: "currentValueWrapper" },
+                label({ class: "labelforcurrvalueofinv" }, block.querySelector('.calContainer10')),
+                span({ class: "currentValue" }, block.querySelector('.calContainer11').textContent.trim() || '0'),
 
-    const defaultFund = config.defaultfund || 'Motilal Oswal Mid cap fund';
+            ),
+            div({ class: "returnCagrWrapper" },
+                label({ class: "labelforretrun" }, block.querySelector('.calContainer12')),
+                span({ class: "returnCagr" }, block.querySelector('.calContainer13').textContent.trim() || '0'),
+            ),
+            div({ class: "startSipbtn" },
+                // block.querySelector('.calContainer17'),
+                button(block.querySelector('.calContainer17').textContent.trim()),
+            )
+        )
+    )
+    let viewOthCalBtn = div({ class: "viewBtnCal" },
+        a({ href: block.querySelector('.calContainer18').textContent.trim() || '#', class: "viewOtherCalBtn" }, block.querySelector('.calContainer18').textContent.trim() || 'View other calculators')
+    );
+    block.innerHTML = '';
+    block.append(calContainer, viewOthCalBtn);
+    
+
+}
+
+
+
+
+// const defaultFund = config.defaultfund || 'Motilal Oswal Mid cap fund';
     // const buttonText = config.buttontext || 'Start SIP';
     // const calculatorLink = config.calculatorlink || 'View other calculators';
     // const calculatorUrl = config.calculatorurl || '#';
@@ -31,5 +99,3 @@ export default function decorate(block) {
     // Current Value of Investment
     // Return (CAGR)
     // Start SIP 
-
-}
