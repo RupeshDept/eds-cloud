bc674d027b9856b037274005c0e74b58c7a88f8d
AnuragPandey <anurag.pandey@teknopoint.in>
2025-07-15 19:15:31 +0530

    added css

---
+++ blocks/cornerstones-advantage-cards/cornerstones-advantage-cards.js
@@ -0,0 +1,6 @@
+export default function decorate(block) {
+  const cardsChildren = block.querySelectorAll(".cornerstones-advantage-cards > div");
+  cardsChildren.forEach((ele)=>{
+    ele.classList.add('cards-item')
+  })
+}
